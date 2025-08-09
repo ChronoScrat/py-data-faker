75b8fef3520ae632a2c49d25a79711b5a7132182
Nathanael Rolim <natanael.rolim@hotmail.com>
2025-08-09 17:16:19 -0300

    Add unit and integration tests for Fixed and Expression-type columns
    This commit adds unit tests for `SchemaColumnFixed` and `SchemaColumnExpression`, and both unit and integration tests for `parse_column_type()` for Fixed
    and Expression-type columns.
    
    For Fixed columns, I noted a design choice that I will leave to the future: currently (and in the original implementation in Scala), leaving the `value`
    field empty for Fixed columns results in an error. However, that is nothing inherently wrong with passing something like `None` to Spark's `lit()`. It would
    only result in a column of `null` values. I have added a TODO to return to this topic at a later date.

--- tests/test_EXPRESSION.py
+++ tests/test_EXPRESSION.py
@@ -1,9 +1,30 @@
-# Testing suite for `parse_colum_type()` from SchemaParser.py
-# Column Type: EXPRESSION
+# Testing suite for column type: EXPRESSION
 
 from datafaker.SchemaParser import *
 import pytest
 
+# Unit tests for SchemaColumnExpression
+
+def test_SchemaColumnExpression_valid():
+    column = SchemaColumnExpression(name = "column_name", expression="rand()")
+    assert isinstance(column, SchemaColumnExpression)
+    assert vars(column) == {"_name" : "column_name", "_expression" : "rand()"}
+
+@pytest.mark.parametrize("input",[
+    (10),
+    (5.1),
+    (True),
+    (datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0))
+])
+def test_SchemaColumnExpression_non_str(input):
+    with pytest.raises(TypeError):
+        SchemaColumnExpression(name = "column_test", expression = input)
+
+def test_SchemaColumnExpression_missin_expr():
+    with pytest.raises(TypeError):
+        SchemaColumnExpression(name = "column_name")
+
+# Unit tests for `parse_column_type()` for Expression Columns
 
 column_expression_valid = {
     "name": "column_test",
@@ -26,3 +47,66 @@ def test_parse_column_expression_valid():
 def test_parse_column_expression_missing_expr():
     with pytest.raises(ValueError):
         parse_column_type(column_expression_missing_expr)
+
+# Integration tests for `parse_column_type()` for Expression columns
+
+column_expression_non_str_int = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : 500
+}
+
+column_expression_non_str_float = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : 5.1
+}
+
+column_expression_non_str_bool = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : True
+}
+
+column_expression_non_str_time = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : datetime.fromisoformat("2025-01-01T00:00:00Z")
+}
+
+column_expression_expr_list_str = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : ["rand()", "lit(2)"]
+}
+
+column_expression_expr_whitespace = {
+    "name": "column_test",
+    "data_type": "Double",
+    "column_type" : "Expression",
+    "expression" : "    "
+}
+
+@pytest.mark.parametrize("input",[
+    (column_expression_non_str_int),
+    (column_expression_non_str_float),
+    (column_expression_non_str_bool),
+    (column_expression_non_str_time)
+])
+def test_parse_column_expression_non_str_expr(input):
+    with pytest.raises(TypeError):
+        parse_column_type(input)
+
+def test_parse_column_expression_expr_list_str():
+    with pytest.raises(TypeError):
+        parse_column_type(column_expression_expr_list_str)
+
+# TODO Possibly treat whitespace-only expressions as empty
+def test_parse_column_expression_expr_whitespace():
+    column = parse_column_type(column_expression_expr_whitespace)
+    assert isinstance(column, SchemaColumnExpression)
--- tests/test_FIXED.py
+++ tests/test_FIXED.py
@@ -1,9 +1,43 @@
-# Testing suite for `parse_colum_type()` from SchemaParser.py
-# Column Type: FIXED
+# Testing suite for column type: EXPRESSION
 
 from datafaker.SchemaParser import *
 import pytest
 
+# Unit tests for SchemaColumnFixed
+
+def test_SchemaColumnFixed_valid_str():
+    column = SchemaColumnFixed(name = "column_test", value = "test")
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == {"_name" : "column_test", "_value" : "test"}
+
+def test_SchemaColumnFixed_valid_int():
+    column = SchemaColumnFixed(name = "column_test", value = 10)
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == {"_name" : "column_test", "_value" : 10}
+
+def test_SchemaColumnFixed_valid_float():
+    column = SchemaColumnFixed(name = "column_test", value = 5.1)
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == {"_name" : "column_test", "_value" : 5.1}
+
+def test_SchemaColumnFixed_valid_datetime():
+    column = SchemaColumnFixed(name = "column_test", value = datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0))
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == {"_name" : "column_test", "_value" : datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)}
+
+def test_SchemaColumnFixed_valid_date():
+    column = SchemaColumnFixed(name = "column_test", value = date(year=2025, month=1, day=1))
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == {"_name" : "column_test", "_value" : date(year=2025, month=1, day=1)}
+
+# NOTE This is different than passing `value=""`
+# TODO I think we should treat both cases as identical and allow for empty values (null in spark)
+def test_SchemaColumnFixed_missing_value():
+    with pytest.raises(TypeError):
+        SchemaColumnFixed(name = "column_test")
+
+
+# Unit tests for `parse_column_type()` for Fixed Columns
 
 column_fixed_valid = {
     "name": "column_test",
@@ -27,3 +61,51 @@ def test_parse_column_fixed_missing_value():
     with pytest.raises(ValueError):
         parse_column_type(column_fixed_missing_value)
 
+# Integration tests for `parse_column_type()` for Fixed columns
+
+column_fixed_valid_int = {
+    "name": "column_test",
+    "data_type": "Int",
+    "column_type" : "Fixed",
+    "value": 10
+}
+
+column_fixed_valid_float = {
+    "name": "column_test",
+    "data_type": "Int",
+    "column_type" : "Fixed",
+    "value": 5.1
+}
+
+column_fixed_valid_string = {
+    "name": "column_test",
+    "data_type": "Int",
+    "column_type" : "Fixed",
+    "value": "string"
+}
+
+column_fixed_valid_datetime = {
+    "name": "column_test",
+    "data_type": "Int",
+    "column_type" : "Fixed",
+    "value": datetime.fromisoformat("2025-01-01T00:00:00")
+}
+
+column_fixed_valid_date = {
+    "name": "column_test",
+    "data_type": "Int",
+    "column_type" : "Fixed",
+    "value": date.fromisoformat("2025-01-01")
+}
+
+@pytest.mark.parametrize("input, result", [
+    (column_fixed_valid_int, {"_name" : "column_test", "_value" : 10}),
+    (column_fixed_valid_float, {"_name" : "column_test", "_value" : 5.1}),
+    (column_fixed_valid_string, {"_name" : "column_test", "_value" : "string"}),
+    (column_fixed_valid_datetime, {"_name" : "column_test", "_value" : datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)}),
+    (column_fixed_valid_date, {"_name" : "column_test", "_value" : date(year=2025, month=1, day=1)})
+])
+def test_parse_column_fixed_val_par(input, result):
+    column = parse_column_type(input)
+    assert isinstance(column, SchemaColumnFixed)
+    assert vars(column) == result
